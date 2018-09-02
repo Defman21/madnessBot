2dd8c39f51b7fbdcceba0a4b3bae2ece2eb0894c
Defman21 <i@defman.me>
2018-09-02 09:29:51 +0300

    Refactored cat command

--- commands/cat.go
+++ commands/cat.go
@@ -2,35 +2,20 @@ package commands
 
 import (
 	"fmt"
-	"github.com/Defman21/madnessBot/common"
-	"github.com/franela/goreq"
+	"strconv"
+	"time"
 	"gopkg.in/telegram-bot-api.v4"
 )
 
 // Cat zulul
 func Cat(bot *tgbotapi.BotAPI, update *tgbotapi.Update) {
-	if !payCheck(bot, update) {
-		return
-	}
-	res, err := goreq.Request{
-		Uri: "https://thecatapi.com/api/images/get",
-		QueryString: struct {
-			Type string
-		}{
-			Type: "jpg,png",
-		},
-	}.Do()
-
-	if err != nil {
-		common.Log.Warn(err)
-		return
-	}
 	msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, nil)
-	msg.FileID = res.Header.Get("Location")
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	msg.FileID = fmt.Sprintf("https://thecatapi.com/api/images/get?type=jpg,png&%s", timestamp)
 	msg.UseExisting = true
-	_, err = bot.Send(msg)
+	_, err := bot.Send(msg)
 	if err != nil {
-		msg := fmt.Sprintf("Все летит в пизду\n%s\nURL: %s", err, res.Header.Get("Location"))
+		msg := fmt.Sprintf("Все летит в пизду\n%s\nURL: %s", err, msg.FileID)
 		bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, msg))
 	}
 }
