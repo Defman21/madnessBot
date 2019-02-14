88274e8ecff6b1a9cff6ae58169a6d58e3929fdc
Defman21 <i@defman.me>
2019-02-14 16:51:39 +0300

    feature(commands, unsubscribe): implement unsubscribe from specific channel
    Fix #3

--- commands/unsubscribe.go
+++ commands/unsubscribe.go
@@ -11,7 +11,16 @@ import (
 	"os"
 )
 
-func Unsubscribe(*tgbotapi.BotAPI, *tgbotapi.Update) {
+func Unsubscribe(bot *tgbotapi.BotAPI, update *tgbotapi.Update) {
+	channel := update.Message.CommandArguments()
+
+	if channel == "" {
+		msg := tgbotapi.NewVoiceShare(update.Message.Chat.ID,
+			"AwADAgADwgADC6ZpS13yfdzm_pTzAg")
+		bot.Send(msg)
+		return
+	}
+
 	bytes, err := ioutil.ReadFile("./data/users.json")
 	if err != nil {
 		common.Log.Warn(err.Error())
@@ -19,10 +28,19 @@ func Unsubscribe(*tgbotapi.BotAPI, *tgbotapi.Update) {
 	}
 
 	var users Users
+	var newUsers Users
+	findUser := false
 
 	json.Unmarshal(bytes, &users)
 
 	for _, user := range users.List {
+		if user[0] != channel {
+			newUsers.List = append(newUsers.List, user)
+			continue
+		} else {
+			findUser = true
+		}
+
 		go func(user []string) {
 			req := goreq.Request{
 				Method: "POST",
@@ -46,9 +64,31 @@ func Unsubscribe(*tgbotapi.BotAPI, *tgbotapi.Update) {
 				common.Log.Warn(err.Error())
 			} else {
 				common.Log.WithFields(logrus.Fields{
-					"user": user[0],
+					"user":    user[0],
+					"context": "commands/unsubscribe",
 				}).Info("Unsubscribed")
+
+				jsonStr, _ := json.Marshal(newUsers)
+
+				err = ioutil.WriteFile("./data/users.json", []byte(jsonStr), 0644)
+				if err == nil {
+					common.Log.Info("Updated users.json")
+					bot.Send(
+						tgbotapi.NewMessage(
+							update.Message.Chat.ID,
+							fmt.Sprintf("Unsubscribed from %s", channel),
+						),
+					)
+				} else {
+					common.Log.Warn("Couldn't write to users.json")
+				}
 			}
 		}(user)
+	}
+
+	if findUser == false {
+		common.Log.WithFields(logrus.Fields{
+			"user": channel,
+		}).Warn("Channel not found")
 	}
 }
