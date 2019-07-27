b12944dd14a97ec54e524cde2b39919cfb27049c
Defman21 <i@defman.me>
2019-07-27 19:47:17 +0300

    feature(commands:cat): send gifs randomly

--- commands/cat/cat.go
+++ commands/cat/cat.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"github.com/Defman21/madnessBot/commands"
 	"github.com/Defman21/madnessBot/common"
+	"github.com/Defman21/madnessBot/common/metrics"
 	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/marpaia/graphite-golang"
+	"math/rand"
 	"strconv"
 	"time"
 )
@@ -25,22 +28,41 @@ func (c *Command) Run(api *tgbotapi.BotAPI, update *tgbotapi.Update) {
 
 	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
 
+	media := &tgbotapi.BaseInputMedia{
+		Type:    "",
+		Media:   "",
+		Caption: "котек найден!",
+	}
+
+	isGif := false
+	if rand.Intn(10) <= 2 {
+		media.Media = fmt.Sprintf("https://thecatapi.com/api/images/get?type=gif&%s", timestamp)
+		media.Type = "animation"
+		isGif = true
+	} else {
+		media.Media = fmt.Sprintf("https://thecatapi.com/api/images/get?type=jpg,png&%s", timestamp)
+		media.Type = "photo"
+	}
+
 	editmsg := tgbotapi.EditMessageMediaConfig{
 		BaseEdit: tgbotapi.BaseEdit{
 			ChatID:    placeholderMessage.Chat.ID,
 			MessageID: placeholderMessage.MessageID,
 		},
-		Media: tgbotapi.BaseInputMedia{
-			Type:    "photo",
-			Media:   fmt.Sprintf("https://thecatapi.com/api/images/get?type=jpg,png&%s", timestamp),
-			Caption: "котек найден!",
-		},
+		Media: media,
 	}
 
 	_, err := api.Send(editmsg)
 
 	if err != nil {
 		common.Log.Error().Err(err).Msg("Failed to edit a message")
+	}
+
+	if isGif {
+		metrics.Graphite().Send(graphite.NewMetric(
+			fmt.Sprintf("stats.gif_cat.%s", update.Message.From.UserName), "1",
+			time.Now().Unix(),
+		))
 	}
 }
 
