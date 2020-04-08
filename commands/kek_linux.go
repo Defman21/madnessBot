3e6aa666e698627cf9c6e2c53037d4f83ca51153
Defman21 <i@defman.me>
2020-04-08 20:57:06 +0300

    Kek images
    Fix #37

--- commands/kek_linux.go
+++ commands/kek_linux.go
@@ -20,45 +20,56 @@ func (c KekCmd) Run(api *tgbotapi.BotAPI, update *tgbotapi.Update) {
 	if !helpers.PayCheck(api, update) {
 		return
 	}
-	photos, err := api.GetUserProfilePhotos(tgbotapi.NewUserProfilePhotos(update.Message.From.ID))
-	if err != nil {
-		logger.Log.Warn().Err(err).Msg("Failed to get user profile photo")
-	} else {
-		direction := update.Message.CommandArguments()
-		zulul := photos.Photos[0]
-		photo := zulul[len(zulul)-1]
-		url, _ := api.GetFileDirectURL(photo.FileID)
-		img, _ := os.Create("zulul.jpg")
-		defer img.Close()
 
-		resp, _ := http.Get(url)
-		defer resp.Body.Close()
-		_, _ = io.Copy(img, resp.Body)
-		imagick.Initialize()
-		defer imagick.Terminate()
-		mw := imagick.NewMagickWand()
-		defer mw.Destroy()
-		mw.ReadImage("zulul.jpg")
-		w := mw.GetImageWidth()
-		h := mw.GetImageHeight()
+	var photo tgbotapi.PhotoSize
 
-		mw.SetImageAlphaChannel(imagick.ALPHA_CHANNEL_DEACTIVATE)
-		if direction == "right" {
-			mw.CropImage(w/2, h, int(w/2), 0)
-			mw.FlopImage()
+	direction := update.Message.CommandArguments()
+
+	if update.Message.ReplyToMessage == nil || len(update.Message.ReplyToMessage.Photo) == 0 {
+		photos, err := api.GetUserProfilePhotos(tgbotapi.NewUserProfilePhotos(update.Message.From.ID))
+		if err != nil {
+			logger.Log.Warn().Err(err).Msg("Failed to get user profile photo")
+			return
 		} else {
-			mw.CropImage(w/2, h, 0, 0)
+			photoSizes := photos.Photos[0]
+			photo = photoSizes[len(photoSizes)-1] // last one is the biggest one
 		}
-		mwr := mw.Clone()
-		defer mwr.Destroy()
-		mwr.FlopImage()
-		mw.AddImage(mwr)
-		mw.SetFirstIterator()
-
-		mwout := mw.AppendImages(false)
-		defer mwout.Destroy()
-		mwout.WriteImage("zulul-done.jpg")
-
-		api.Send(tgbotapi.NewPhotoUpload(update.Message.Chat.ID, "zulul-done.jpg"))
+	} else {
+		photoSizes := update.Message.ReplyToMessage.Photo
+		photo = photoSizes[len(photoSizes)-1] // last one is the biggest one
 	}
+
+	url, _ := api.GetFileDirectURL(photo.FileID)
+	img, _ := os.Create("zulul.jpg")
+	defer img.Close()
+
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	_, _ = io.Copy(img, resp.Body)
+	imagick.Initialize()
+	defer imagick.Terminate()
+	mw := imagick.NewMagickWand()
+	defer mw.Destroy()
+	mw.ReadImage("zulul.jpg")
+	w := mw.GetImageWidth()
+	h := mw.GetImageHeight()
+
+	mw.SetImageAlphaChannel(imagick.ALPHA_CHANNEL_DEACTIVATE)
+	if direction == "right" {
+		mw.CropImage(w/2, h, int(w/2), 0)
+		mw.FlopImage()
+	} else {
+		mw.CropImage(w/2, h, 0, 0)
+	}
+	mwr := mw.Clone()
+	defer mwr.Destroy()
+	mwr.FlopImage()
+	mw.AddImage(mwr)
+	mw.SetFirstIterator()
+
+	mwout := mw.AppendImages(false)
+	defer mwout.Destroy()
+	mwout.WriteImage("zulul-done.jpg")
+
+	api.Send(tgbotapi.NewPhotoUpload(update.Message.Chat.ID, "zulul-done.jpg"))
 }
